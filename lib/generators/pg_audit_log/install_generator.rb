1c53b2bdf805802777b76dc7c2b1fc06ed7677f4
Kris Hicks <krishicks@gmail.com>
2013-07-27 15:31:48 -0400

    Update install generator

--- lib/generators/pg_audit_log/install_generator.rb
+++ lib/generators/pg_audit_log/install_generator.rb
@@ -1,18 +1,25 @@
-require "rails/generators/active_record"
+require 'rails/generators/migration'
+require 'active_record'
 
 module PgAuditLog
   module Generators
     class InstallGenerator < Rails::Generators::Base
       include Rails::Generators::Migration
-      extend ::ActiveRecord::Generators::Migration
 
       source_root File.expand_path('../templates', __FILE__)
 
-      def install
-        directory "lib/tasks"
+      def copy_migration
         migration_template "migration.rb", "db/migrate/install_pg_audit_log"
       end
 
+      def self.next_migration_number(dirname)
+        next_migration_number = current_migration_number(dirname) + 1
+        if ::ActiveRecord::Base.timestamped_migrations
+          [Time.now.utc.strftime("%Y%m%d%H%M%S"), "%.14d" % next_migration_number].max
+        else
+          "%.3d" % next_migration_number
+        end
+      end
     end
   end
 end
